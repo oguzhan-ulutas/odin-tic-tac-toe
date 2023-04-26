e5c2efe3588eded6313787a3f8d802a409427b51
Oguzhan Ulutas <mkoulutas@gmail.com>
2023-04-26 18:54:27 +0300

    Fix restart button functionality

--- script.js
+++ script.js
@@ -1,37 +1,54 @@
 const gameBoard = (() => {
-  const makerBoxDiv = document.querySelectorAll(".game-board div");
-  const markerX = document.querySelector(
-    ".buttons-marker > button:first-child"
-  );
-  const markerO = document.querySelector(".buttons-marker > button:last-child");
   let gameBoardArray = ["X", "O"];
+  let playerMarker = "";
+  let computerMarker = "";
 
   //Filling the game board with gameBoardArray items.
   function fillGameBoard() {
-    if (gameBoardArray === []) {
-      makerBoxDiv.forEach((box) => {
-        box.textContent = "";
-      });
-    } else {
-      for (let index in gameBoardArray) {
-        const div = document.querySelector(`.div${index}`);
-        div.textContent = gameBoard.gameBoardArray[index];
-      }
+    for (let index in gameBoardArray) {
+      const div = document.querySelector(`.div${index}`);
+      div.textContent = gameBoardArray[index];
     }
   }
 
-  // Add restart button functionality
-  const restartButton = document.querySelector(".button-restart > button");
-  restartButton.addEventListener("click", () => {
-    gameBoard.gameBoardArray = [];
-    fillGameBoard();
-  });
+  // function controlFunction() {
+  //   console.log("working");
+  // }
 
-  return { makerBoxDiv, gameBoardArray, fillGameBoard };
+  // Add restart button functionality
+
+  function clearDisplay() {
+    const makerBoxDiv = document.querySelectorAll(".game-board div");
+    makerBoxDiv.forEach((box) => {
+      box.textContent = "";
+    });
+  }
+
+  function restart() {
+    const restartButton = document.querySelector(".button-restart > button");
+    restartButton.addEventListener("click", () => {
+      this.gameBoardArray = [];
+      clearDisplay();
+    });
+  }
+
+  // Choosing marker
+
+  function markerSelect() {
+    const markerX = document.querySelector(
+      ".buttons-marker > button:first-child"
+    );
+    const markerO = document.querySelector(
+      ".buttons-marker > button:last-child"
+    );
+    markerX.addEventListener("click", () => {
+      playerMarker = "X";
+      computerMarker = "O";
+    });
+  }
+
+  return { fillGameBoard, restart, gameBoardArray };
 })();
 
 gameBoard.fillGameBoard();
-
-// gameBoard.makerBoxDiv.forEach((box) => {
-//   console.log(box.textContent);
-// });
+gameBoard.restart();
